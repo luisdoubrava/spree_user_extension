6a946bffdc00b92ddcba2e4cdfc3cec1e70e7e79
Luis Doubrava <ldoubrava@gmail.com>
2012-07-06 11:19:11 +0200

    insert phone_number in top of user admin hook

--- app/overrides/add_phone_number_to_user_admin.rb
+++ app/overrides/add_phone_number_to_user_admin.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/users/_form",
-                     :name => "user_admin_phone_number_form_left",
-                     :insert_after => "p#user_email_field",
+                     :name => "user_admin_phone_number_form",
+                     :insert_top => "[data-hook='admin_user_form_fields']",
                      :partial => "spree/admin/users/phone_number_field",
                      :disabled => false)
