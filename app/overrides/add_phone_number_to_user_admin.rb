69d85e1f5acb8846cb0799fe3c6aed457c447877
Luis Doubrava <ldoubrava@gmail.com>
2012-07-06 10:52:20 +0200

    fix deface call

--- app/overrides/add_phone_number_to_user_admin.rb
+++ app/overrides/add_phone_number_to_user_admin.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/users/_form",
                      :name => "user_admin_phone_number_form_left",
-                     :insert_after => "[p#user_email_field]",
+                     :insert_after => "p#user_email_field",
                      :partial => "spree/admin/users/phone_number_field",
                      :disabled => false)
