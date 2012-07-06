4a7788f7995cc78adf082d77ea5885a7459e5cc0
Luis Doubrava <ldoubrava@gmail.com>
2012-07-06 13:02:23 +0200

    add optional phone_number to creation of anonymous user

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,4 +1,8 @@
 Spree::User.class_eval do
   attr_accessible :phone_number
   
+  def self.anonymous!(phone_number = nil)
+    token = User.generate_token(:persistence_token)
+    User.create(:email => "#{token}@example.net", :phone_number => "#{phone_number}", :password => token, :password_confirmation => token, :persistence_token => token)
+  end
 end
